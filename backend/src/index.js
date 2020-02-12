397a5a497d2578ba89912bfcaf4b2de418f94309
Pedro Paulo Andrade Faria <pedroandradef@outlook.com>
2020-02-12 16:14:25 -0300

    Initial commit

---
+++ backend/src/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+const app = express();
+
+mongoose.connect('mongodb://pedro:pedro@mybd-shard-00-00-msw2k.gcp.mongodb.net:27017,mybd-shard-00-01-msw2k.gcp.mongodb.net:27017,mybd-shard-00-02-msw2k.gcp.mongodb.net:27017/week10?ssl=true&replicaSet=myBD-shard-0&authSource=admin&retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+app.use(express.json());
+app.use(routes);
+app.listen(3333);
---
+++ backend/src/routes.js
@@ -0,0 +1,10 @@
+const { Router } = require('express');
+const devController = require('./controllers/devController');
+
+
+const routes = Router();
+
+routes.post('/devs', devController.store)
+    
+
+module.exports = routes;
